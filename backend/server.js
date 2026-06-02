ca6a8d3513bc6b4ee4ce2a981fb2e7254ac3f592
Anshu raj <anshur0202@gmail.com>
2026-06-03 00:49:16 +0530

    seconf commit23

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(
     origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-app-client"],
   })
 );
 
